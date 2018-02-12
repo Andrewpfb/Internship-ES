cd6ae85bb0c4b6317fe4ebae85ef7d440d3f1786
ES\andrey.golik <andrey.golik@effective-soft.com>
2018-02-12 14:28:09 +0300

    fix table show

--- MapsProject/MapsAngular/src/app/_administration/moderate/moderate.component.ts
+++ MapsProject/MapsAngular/src/app/_administration/moderate/moderate.component.ts
@@ -23,9 +23,9 @@ import { Data } from '../_models/data';
 })
 export class ModerateComponent implements OnInit {
   displayedColumns = ['Id', 'Name', 'Tags', 'Latitude', 'Longitude', 'Status', 'Delete', 'Approve'];
-  // dataSource = new MatTableDataSource();
-  dataSource = ModerateDataSource;
-  moderateDatabase = new ModerateDatabase();
+  dataSource = new MatTableDataSource();
+  // dataSource = ModerateDataSource;
+  // moderateDatabase = new ModerateDatabase();
   data = [];
 
   resultsLength = 0;
@@ -47,45 +47,45 @@ export class ModerateComponent implements OnInit {
 
   approvedObject(id) {
     this.dataService.approvedObject(id).subscribe(data => console.log('Approved'));
-    //this.loadDataFromService();
-    //this.loadDataToTable();
+    this.loadDataFromService();
+    this.loadDataToTable();
   }
 
   ngOnInit() {
-    this.dataSource = new ModerateDataSource(this.moderateDatabase);
-    //this.loadDataFromService();
-    //this.loadDataToTable();
+   // this.dataSource = new ModerateDataSource(this.moderateDatabase);
+    this.loadDataFromService();
+    this.loadDataToTable();
   }
 
   loadDataFromService() {
     this.dataService.getData().subscribe(data => {
       this.data = data;
-    })
+    });
   }
 
-  // loadDataToTable() {
-  //   this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+  loadDataToTable() {
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
-  //   merge(this.sort.sortChange, this.paginator.page)
-  //     .pipe(
-  //     startWith({}),
-  //     switchMap(() => {
-  //       this.isLoadingResults = true;
-  //       //return this.data;
-  //       return this.dataService.getData();
-  //     }),
-  //     map(data => {
-  //       this.isLoadingResults = false;
-  //       this.resultsLength = data.length;
-  //       this.dataSource.sort = this.sort;
-  //       return data;
-  //     }),
-  //     catchError(() => {
-  //       this.isLoadingResults = false;
-  //       return observableOf([]);
-  //     })
-  //     ).subscribe(data => this.dataSource.data = data);
-  // }
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults = true;
+        // return this.data;
+        return this.dataService.getData();
+      }),
+      map(data => {
+        this.isLoadingResults = false;
+        this.resultsLength = data.length;
+        this.dataSource.sort = this.sort;
+        return data;
+      }),
+      catchError(() => {
+        this.isLoadingResults = false;
+        return observableOf([]);
+      })
+      ).subscribe(data => this.dataSource.data = data);
+  }
 
   logout() {
     this.authenticationService.logout();
@@ -93,21 +93,21 @@ export class ModerateComponent implements OnInit {
   }
 }
 
-export class ModerateDatabase {
-  dataChange: BehaviorSubject<Data[]> = new BehaviorSubject<Data[]>([]);
-  get data(): Data[] { return this.dataChange.value};
+// export class ModerateDatabase {
+//   dataChange: BehaviorSubject<Data[]> = new BehaviorSubject<Data[]>([]);
+//   get data(): Data[] { return this.dataChange.value};
 
-  constructor(){}
-}
+//   constructor(){}
+// }
 
-export class ModerateDataSource extends DataSource<any> {
-  constructor (private moderateService: ModerateDataService){
-    super();
-  }
+// export class ModerateDataSource extends DataSource<any> {
+//   constructor (private moderateService: ModerateDataService){
+//     super();
+//   }
 
-  connect(): Observable<Data[]>{
-    return this.moderateService.getData();
-  }
+//   connect(): Observable<Data[]>{
+//     return this.moderateService.getData();
+//   }
 
-  disconnect(){}
-}
+//   disconnect(){}
+// }
