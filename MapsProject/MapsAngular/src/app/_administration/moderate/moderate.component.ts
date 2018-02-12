3dbb29b1c8fc7e67f4843f0474ae040109eb06fb
ES\andrey.golik <andrey.golik@effective-soft.com>
2018-02-12 14:35:29 +0300

    del old code, add dynamic update after delete or approve

--- MapsProject/MapsAngular/src/app/_administration/moderate/moderate.component.ts
+++ MapsProject/MapsAngular/src/app/_administration/moderate/moderate.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { Observable } from 'rxjs/Observable';
+
 import { merge } from 'rxjs/observable/merge';
 import { of as observableOf } from 'rxjs/observable/of';
 import { catchError } from 'rxjs/operators/catchError';
 import { map } from 'rxjs/operators/map';
 import { startWith } from 'rxjs/operators/startWith';
 import { switchMap } from 'rxjs/operators/switchMap';
+
 import { AuthenticationService } from '../_services/authentication.service';
-import { Router } from '@angular/router';
 import { ModerateDataService } from '../_services/moderate-data.service';
-import { DataSource } from '@angular/cdk/collections';
-import { BehaviorSubject } from 'rxjs';
-import { Data } from '../_models/data';
+
 
 @Component({
   selector: 'app-moderate',
@@ -24,9 +23,6 @@ import { Data } from '../_models/data';
 export class ModerateComponent implements OnInit {
   displayedColumns = ['Id', 'Name', 'Tags', 'Latitude', 'Longitude', 'Status', 'Delete', 'Approve'];
   dataSource = new MatTableDataSource();
-  // dataSource = ModerateDataSource;
-  // moderateDatabase = new ModerateDatabase();
-  data = [];
 
   resultsLength = 0;
   isLoadingResults = true;
@@ -35,35 +31,30 @@ export class ModerateComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(
-    private http: HttpClient,
     private authenticationService: AuthenticationService,
     private dataService: ModerateDataService,
     private router: Router) {
   }
 
   deleteObject(id) {
-    this.dataService.deleteData(id).subscribe(data => console.log('Delete'));
-  }
-
-  approvedObject(id) {
-    this.dataService.approvedObject(id).subscribe(data => console.log('Approved'));
-    this.loadDataFromService();
-    this.loadDataToTable();
-  }
-
-  ngOnInit() {
-   // this.dataSource = new ModerateDataSource(this.moderateDatabase);
-    this.loadDataFromService();
-    this.loadDataToTable();
-  }
-
-  loadDataFromService() {
-    this.dataService.getData().subscribe(data => {
-      this.data = data;
+    this.dataService.deleteData(id).subscribe(data => {
+      console.log('Delete');
+      this.loadData();
     });
   }
 
-  loadDataToTable() {
+  approvedObject(id) {
+    this.dataService.approvedObject(id).subscribe(data => {
+      console.log('Approved');
+      this.loadData();
+    });
+  }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
     merge(this.sort.sortChange, this.paginator.page)
@@ -71,7 +62,6 @@ export class ModerateComponent implements OnInit {
       startWith({}),
       switchMap(() => {
         this.isLoadingResults = true;
-        // return this.data;
         return this.dataService.getData();
       }),
       map(data => {
@@ -92,22 +82,3 @@ export class ModerateComponent implements OnInit {
     this.router.navigate(['']);
   }
 }
-
-// export class ModerateDatabase {
-//   dataChange: BehaviorSubject<Data[]> = new BehaviorSubject<Data[]>([]);
-//   get data(): Data[] { return this.dataChange.value};
-
-//   constructor(){}
-// }
-
-// export class ModerateDataSource extends DataSource<any> {
-//   constructor (private moderateService: ModerateDataService){
-//     super();
-//   }
-
-//   connect(): Observable<Data[]>{
-//     return this.moderateService.getData();
-//   }
-
-//   disconnect(){}
-// }
